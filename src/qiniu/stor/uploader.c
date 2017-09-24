6dd69761fda7c3cda35bc37b7f5c68fde8dd94d5
LIANG Tao <amethyst.black@gmail.com>
2017-09-24 22:38:32 +0800

    添加代码。

--- src/qiniu/stor/uploader.c
+++ src/qiniu/stor/uploader.c
@@ -79,9 +79,94 @@ QN_SDK void qn_up_destroy(qn_uploader_ptr restrict up)
     } // if
 }
 
+/* -- Extra Arugment Setting methods -- */ 
+
+QN_SDK qn_bool qn_up_set_mime_type(qn_json_object_ptr restrict ext, const char * restrict mime_type)
+{
+    assert(ext);
+    assert(mime_type);
+    return qn_json_set_cstr(ext, "mime_type", mime_type);
+}
+
+QN_SDK qn_bool qn_up_set_accept_type(qn_json_object_ptr restrict ext, const char * restrict accept_type)
+{
+    assert(ext);
+    assert(accept_type);
+    return qn_json_set_cstr(ext, "accept_type", accept_type);
+}
+
+QN_SDK qn_bool qn_up_set_crc32(qn_json_object_ptr restrict ext, const char * restrict crc32)
+{
+    assert(ext);
+    assert(crc32);
+    return qn_json_set_cstr(ext, "crc32", crc32);
+}
+
+QN_SDK qn_bool qn_up_set_save_key(qn_json_object_ptr restrict ext, const char * restrict save_key)
+{
+    assert(ext);
+    assert(save_key);
+    return qn_json_set_cstr(ext, "save_key", save_key);
+}
+
+QN_SDK qn_bool qn_up_set_user_defined_value(qn_json_object_ptr restrict ext, const char * restrict key, const char * val)
+{
+    qn_json_object ud = NULL;
+    assert(ext);
+    assert(key && val);
+    if (! (ud = qn_json_get_object(ext, "user_defined_vars", NULL))) {
+        if (! (ud = qn_json_create_and_set_object(ext, "user_defined_vars"))) return qn_false;
+    } // if
+    return qn_json_set_cstr(ud, key, val);
+}
+
+QN_SDK qn_bool qn_up_set_service_entry(qn_json_object_ptr restrict ext, qn_svc_entry_ptr restrict entry)
+{
+    assert(ext);
+    assert(entry);
+    if (! qn_json_set_cstr(ext, "base_url", entry->base_url)) return qn_false;
+    if (entry->hostname && ! qn_json_set_cstr(ext, "hostname", entry->hostname)) return qn_false;
+    return qn_true;
+}
+
 /* -- API methods -- */ 
 
-QN_SDK extern qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, const char * restrict uptoken, qn_io_reader_itf restrict data_rdr, qn_json_object_ptr restrict upe)
+static qn_bool qn_up_set_arguments(qn_http_form_ptr restrict form, qn_json_object_ptr restrict ext)
+{
+    int i = 0;
+    qn_string tmp = NULL;
+    qn_json_array_ptr arr = NULL;
+    qn_json_iterator_ptr itr = NULL;
+
+    if ((tmp = qn_json_get_string(ext, "mime_type", NULL))) {
+        if (! qn_http_form_add_string(form, "mime_type", tmp)) return qn_false;
+    } // if
+
+    if ((tmp = qn_json_get_string(ext, "accept_type", NULL))) {
+        if (! qn_http_form_add_string(form, "accept_type", tmp)) return qn_false;
+    } // if
+
+    if ((tmp = qn_json_get_string(ext, "crc32", NULL))) {
+        if (! qn_http_form_add_string(form, "crc32", tmp)) return qn_false;
+    } // if
+
+    if ((tmp = qn_json_get_string(ext, "save_key", NULL))) {
+        if (! qn_http_form_add_string(form, "save_key", tmp)) return qn_false;
+    } // if
+
+    if ((arr = qn_json_get_array(ext, "user_defined_vars", NULL))) {
+        if (! (itr = qn_json_itr_create())) return NULL;
+
+        if (! qn_json_itr_push_array(itr, arr)) {
+            qn_json_itr_destroy(itr);
+            return NULL;
+        } // if
+
+        // TODO: Add user-defined variables.
+    } // if
+}
+
+QN_SDK qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, const char * restrict uptoken, qn_io_reader_itf restrict data_rdr, qn_json_object_ptr restrict ext)
 {
     qn_bool ok = qn_false;
     const char * mime_type = NULL;
@@ -94,12 +179,12 @@ QN_SDK extern qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, c
     assert(uptoken);
     assert(data_rdr);
 
-    if (upe) {
-        extern_ent.base_url = qn_json_get_string(upe, "base_url", NULL);
-        extern_ent.hostname = qn_json_get_string(upe, "hostname", NULL);
+    if (ext) {
+        extern_ent.base_url = qn_json_get_string(ext, "base_url", NULL);
+        extern_ent.hostname = qn_json_get_string(ext, "hostname", NULL);
         ent = &extern_ent;
 
-        mime_type = upe->mime_type;
+        mime_type = ext->mime_type;
     } // if
     if (! ent->base_url) ent = qn_svc_sel_next_entry(up->sel);
 
@@ -114,7 +199,9 @@ QN_SDK extern qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, c
     if (ent->hostname && ! qn_rpc_set_request_header(up->rpc, "Host", qn_str_cstr(ent->hostname))) return NULL;
     ret = qn_rpc_post_form(up->rpc, qn_str_cstr(ent->base_url), form);
     if (! ret) {
-        if (ent != &extern_ent) qn_http_check_and_register_connection_failure(up->sel, ent);
+        if (ent != &extern_ent && qn_http_failed_in_communication()) {
+            qn_svc_sel_register_failed_entry(up->sel, ent);
+        } // if
         return NULL;
     } // if
     return ret;
@@ -122,12 +209,25 @@ QN_SDK extern qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, c
 
 /* -- API Wrapper methods -- */ 
 
-QN_SDK extern qn_json_object_ptr qn_up_upload_file(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict fname, qn_json_object_ptr restrict ext)
+QN_SDK qn_json_object_ptr qn_up_upload_file(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict fname, qn_json_object_ptr restrict ext)
 {
+    qn_bool ok = qn_false;
+    qn_json_object_ptr ret = NULL;
+    qn_file_ptr fl = NULL;
+
+    assert(up);
+    assert(uptoken);
+    assert(fname);
+
+    if (! (fl = qn_fl_open(fname, NULL))) return NULL;
+    ret = qn_up_api_upload(up, uptoken, qn_fl_to_io_reader(fl), ext);
+    qn_fl_close(fl);
+    return ret;
 }
 
-QN_SDK extern qn_json_object_ptr qn_up_upload_buffer(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict buf, qn_size buf_size, qn_json_object_ptr restrict ext)
+QN_SDK qn_json_object_ptr qn_up_upload_buffer(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict buf, qn_size buf_size, qn_json_object_ptr restrict ext)
 {
+    return NULL;
 }
 
 #ifdef __cplusplus
--- src/qiniu/stor/uploader.h
+++ src/qiniu/stor/uploader.h
@@ -14,56 +14,6 @@ extern "C"
 {
 #endif
 
-/* ---- Declaration of Uploader Extra Argument (Abbreviation: upe) ---- */
-
-static inline qn_bool qn_upe_set_mime_type(qn_json_object_ptr restrict upe, const char * restrict mime_type)
-{
-    assert(upe);
-    assert(mime_type);
-    return qn_json_set_cstr(upe, "mime_type", mime_type);
-}
-
-static inline qn_bool qn_upe_set_accept_type(qn_json_object_ptr restrict upe, const char * restrict accept_type)
-{
-    assert(upe);
-    assert(accept_type);
-    return qn_json_set_cstr(upe, "accept_type", accept_type);
-}
-
-static inline qn_bool qn_upe_set_crc32(qn_json_object_ptr restrict upe, const char * restrict crc32)
-{
-    assert(upe);
-    assert(crc32);
-    return qn_json_set_cstr(upe, "crc32", crc32);
-}
-
-static inline qn_bool qn_upe_set_save_key(qn_json_object_ptr restrict upe, const char * restrict save_key)
-{
-    assert(upe);
-    assert(save_key);
-    return qn_json_set_cstr(upe, "save_key", save_key);
-}
-
-static inline qn_bool qn_upe_set_service_entry(qn_json_object_ptr restrict upe, qn_svc_entry_ptr restrict entry)
-{
-    assert(upe);
-    assert(entry);
-    if (! qn_json_set_cstr(upe, "base_url", entry->base_url)) return qn_false;
-    if (entry->hostname && ! qn_json_set_cstr(upe, "hostname", entry->hostname)) return qn_false;
-    return qn_true;
-}
-
-static inline qn_bool qn_upe_set_user_defined_value(qn_json_object_ptr restrict upe, const char * restrict key, const char * val)
-{
-    qn_json_object ud = NULL;
-    assert(upe);
-    assert(key && val);
-    if (! (ud = qn_json_get_object(upe, "user_defined_vars", NULL))) {
-        if (! (ud = qn_json_create_and_set_object(upe, "user_defined_vars"))) return qn_false;
-    } // if
-    return qn_json_set_cstr(ud, key, val);
-}
-
 /* ---- Declaration of Uploader (Abbreviation: up) ---- */
 
 struct _QN_UPLOADER;
@@ -75,14 +25,24 @@ QN_SDK extern qn_uploader_ptr qn_up_create(void);
 QN_SDK extern qn_uploader_ptr qn_up_create_ex(qn_rpc_ptr restrict rpc, qn_svc_selector_ptr restrict sel);
 QN_SDK extern void qn_up_destroy(qn_uploader_ptr restrict up);
 
+/* -- Extra Arugment Setting methods -- */ 
+
+QN_SDK extern qn_bool qn_up_set_mime_type(qn_json_object_ptr restrict ext, const char * restrict mime_type);
+QN_SDK extern qn_bool qn_up_set_accept_type(qn_json_object_ptr restrict ext, const char * restrict accept_type);
+QN_SDK extern qn_bool qn_up_set_crc32(qn_json_object_ptr restrict ext, const char * restrict crc32);
+QN_SDK extern qn_bool qn_up_set_save_key(qn_json_object_ptr restrict ext, const char * restrict save_key);
+QN_SDK extern qn_bool qn_up_set_user_defined_value(qn_json_object_ptr restrict ext, const char * restrict key, const char * val);
+
+QN_SDK qn_bool qn_up_set_service_entry(qn_json_object_ptr restrict ext, qn_svc_entry_ptr restrict entry);
+
 /* -- API methods -- */ 
 
-QN_SDK extern qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, const char * restrict uptoken, qn_io_reader_itf restrict data_rdr, qn_json_object_ptr restrict upe);
+QN_SDK extern qn_json_object_ptr qn_up_api_upload(qn_uploader_ptr restrict up, const char * restrict uptoken, qn_io_reader_itf restrict data_rdr, qn_json_object_ptr restrict ext);
 
 /* -- API Wrapper methods -- */ 
 
-QN_SDK extern qn_json_object_ptr qn_up_upload_file(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict fname, qn_json_object_ptr restrict upe);
-QN_SDK extern qn_json_object_ptr qn_up_upload_buffer(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict buf, qn_size buf_size, qn_json_object_ptr restrict upe);
+QN_SDK extern qn_json_object_ptr qn_up_upload_file(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict fname, qn_json_object_ptr restrict ext);
+QN_SDK extern qn_json_object_ptr qn_up_upload_buffer(qn_uploader_ptr restrict up, const char * restrict uptoken, const char * restrict buf, qn_size buf_size, qn_json_object_ptr restrict ext);
 
 #ifdef __cplusplus
 }
